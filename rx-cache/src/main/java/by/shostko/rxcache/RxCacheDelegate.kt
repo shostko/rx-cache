81113a8d3d5077582eb97e8728803c9823f9cef9
Sergey Shostko <siarhei.shostka@instinctools.com>
2019-10-08 15:51:26 +0300

    more delegated and ext functions

--- rx-cache/src/main/java/by/shostko/rxcache/RxCacheDelegate.kt
+++ rx-cache/src/main/java/by/shostko/rxcache/RxCacheDelegate.kt
@@ -1,4 +1,4 @@
-@file:Suppress("unused")
+@file:Suppress("unused", "MoveLambdaOutsideParentheses")
 
 package by.shostko.rxcache
 
@@ -15,15 +15,45 @@ import io.reactivex.subjects.Subject
 import kotlin.properties.ReadOnlyProperty
 import kotlin.reflect.KProperty
 
+fun <T> Flowable<T>.cache(lifecycle: Lifecycle): Flowable<T> = FlowableCache(lifecycle, { this }).getValue()
+
+fun <T> Flowable<T>.cache(lifecycleOwner: LifecycleOwner): Flowable<T> = FlowableCache(lifecycleOwner.lifecycle, { this }).getValue()
+
+fun <T> Observable<T>.cache(lifecycle: Lifecycle): Observable<T> = ObservableCache(lifecycle, { this }).getValue()
+
+fun <T> Observable<T>.cache(lifecycleOwner: LifecycleOwner): Observable<T> = ObservableCache(lifecycleOwner.lifecycle, { this }).getValue()
+
+fun <T> Single<T>.cache(lifecycle: Lifecycle): Single<T> = SingleCache(lifecycle, { this }).getValue()
+
+fun <T> Single<T>.cache(lifecycleOwner: LifecycleOwner): Single<T> = SingleCache(lifecycleOwner.lifecycle, { this }).getValue()
+
+fun <T> Maybe<T>.cache(lifecycle: Lifecycle): Maybe<T> = MaybeCache(lifecycle, { this }).getValue()
+
+fun <T> Maybe<T>.cache(lifecycleOwner: LifecycleOwner): Maybe<T> = MaybeCache(lifecycleOwner.lifecycle, { this }).getValue()
+
+fun Completable.cache(lifecycle: Lifecycle): Completable = CompletableCache(lifecycle, { this }).getValue()
+
+fun Completable.cache(lifecycleOwner: LifecycleOwner): Completable = CompletableCache(lifecycleOwner.lifecycle, { this }).getValue()
+
 fun <T> LifecycleOwner.flowable(initializer: () -> Flowable<T>): ReadOnlyProperty<LifecycleOwner, Flowable<T>> = FlowableCache(lifecycle, initializer)
+
+fun <T> Lifecycle.flowable(initializer: () -> Flowable<T>): ReadOnlyProperty<LifecycleOwner, Flowable<T>> = FlowableCache(this, initializer)
 
 fun <T> LifecycleOwner.observable(initializer: () -> Observable<T>): ReadOnlyProperty<LifecycleOwner, Observable<T>> = ObservableCache(lifecycle, initializer)
 
+fun <T> Lifecycle.observable(initializer: () -> Observable<T>): ReadOnlyProperty<LifecycleOwner, Observable<T>> = ObservableCache(this, initializer)
+
 fun <T> LifecycleOwner.single(initializer: () -> Single<T>): ReadOnlyProperty<LifecycleOwner, Single<T>> = SingleCache(lifecycle, initializer)
+
+fun <T> Lifecycle.single(initializer: () -> Single<T>): ReadOnlyProperty<LifecycleOwner, Single<T>> = SingleCache(this, initializer)
 
 fun <T> LifecycleOwner.maybe(initializer: () -> Maybe<T>): ReadOnlyProperty<LifecycleOwner, Maybe<T>> = MaybeCache(lifecycle, initializer)
 
+fun <T> Lifecycle.maybe(initializer: () -> Maybe<T>): ReadOnlyProperty<LifecycleOwner, Maybe<T>> = MaybeCache(this, initializer)
+
 fun LifecycleOwner.completable(initializer: () -> Completable): ReadOnlyProperty<LifecycleOwner, Completable> = CompletableCache(lifecycle, initializer)
+
+fun Lifecycle.completable(initializer: () -> Completable): ReadOnlyProperty<LifecycleOwner, Completable> = CompletableCache(this, initializer)
 
 class FlowableCache<T>(lifecycle: Lifecycle, initializer: () -> Flowable<T>) : RxCache<Flowable<T>, FlowableProcessor<T>>(lifecycle, initializer) {
 
@@ -86,7 +116,7 @@ abstract class RxCache<T, H>(lifecycle: Lifecycle, initializer: () -> T) : ReadO
     private var initializer: (() -> T)? = initializer
     private var holder: H? = null
 
-    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+    internal fun getValue(): T {
         if (holder == null) {
             synchronized(this) {
                 if (holder == null) {
@@ -108,6 +138,8 @@ abstract class RxCache<T, H>(lifecycle: Lifecycle, initializer: () -> T) : ReadO
         }
         return finalize(holder!!)
     }
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): T = getValue()
 
     private fun initialize(): T {
         val result = initializer!!()
